d508d9a4898175b302384169e15953e34ae20bb3
lbovboe <jianbo2@hotmail.com>
2022-10-28 23:27:17 +0800

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,59 @@
+import pyautogui
+from pynput.keyboard import *
+
+#  ======== settings ========
+delay = 30  # in seconds
+delay2 = 1
+resume_key = Key.f1
+pause_key = Key.f2
+exit_key = Key.esc
+#  ==========================
+
+pause = True
+running = True
+
+def on_press(key):
+    global running, pause
+
+    if key == resume_key:
+        pause = False
+        print("[Resumed]")
+    elif key == pause_key:
+        pause = True
+        print("[Paused]")
+    elif key == exit_key:
+        running = False
+        print("[Exit]")
+
+
+def display_controls():
+    print("// AutoClicker by Paul")
+    print("// - Settings: ")
+    print("\t delay = " + str(delay) + ' sec' + '\n')
+    print("// - Controls:")
+    print("\t F1 = Resume")
+    print("\t F2 = Pause")
+    print("\t F3 = Exit")
+    print("-----------------------------------------------------")
+    print('Press F1 to start ...')
+
+
+def main():
+    lis = Listener(on_press=on_press)
+    lis.start()
+
+    display_controls()
+    while running:
+        if not pause:
+            # pyautogui.click(pyautogui.position())
+            # pyautogui.PAUSE = delay2
+            for i in range(30):
+                pyautogui.press('s')
+                pyautogui.press('s')
+                pyautogui.PAUSE = delay2
+            pyautogui.press('e')
+    lis.stop()
+
+
+if __name__ == "__main__":
+    main()
