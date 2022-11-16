61f4c572cd6d33be812b3d58b5fb0645aeeaa26d
lbovboe <jianbo2@hotmail.com>
2022-11-16 11:40:31 +0800

    Update Word_Guesser.py
    Allow user to guess the whole word also 1 alphabet

--- Lesson5/Word_Guesser.py
+++ Lesson5/Word_Guesser.py
@@ -21,24 +21,30 @@ print(*guesser_container)
 # set for 3 lives
 lives = 3
 while True:
-    alphabet = input('Enter your guess alphabet : ')
-    if len(alphabet) != 1:
-        print('Please Enter only 1 alphabet')
-        continue
-
-    if alphabet in random_word:
-        # find the indexes that appear inside this random word
-        for i in range(word_length):
-            if alphabet == random_word[i]:
-                guesser_container[i] = alphabet
+    alphabet = input('Enter your guess alphabet/word : ')
+    
+    # if the user only entered 1 letter
+    if len(alphabet) == 1:
+        if alphabet in random_word:
+            # find the indexes that appear inside this random word
+            for i in range(word_length):
+                if alphabet == random_word[i]:
+                    guesser_container[i] = alphabet
+        else:
+            print('Wrong guess ! Guess Again.')
+            lives-=1
     else:
-        print('Wrong guess ! Guess Again.')
-        lives-=1
+        # when user wants to key in the whole word
+        if random_word == alphabet:
+            print('-------You Won ------')
+            break
+            
     
     print(*guesser_container)
     # break when all the dash are filled up
     if '-' not in  guesser_container:
         print('-------You Won ------')
+        break
     if lives == 0:
         print("----You Lost No more Lives -----")
         break
