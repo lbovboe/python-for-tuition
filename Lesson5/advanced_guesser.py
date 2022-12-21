917f0e3fa0a74407b68af96fc2f3845b34ccb6cf
lbovboe <jianbo2@hotmail.com>
2022-12-21 18:13:15 +0800

    Update advanced_guesser.py
    update guesser

--- Lesson5/advanced_guesser.py
+++ Lesson5/advanced_guesser.py
@@ -1,7 +1,10 @@
 import random
 import time
+import copy
 word_pool = []
 d = {}
+for i in range(100):
+    print()
 number_players = int(input('Enter the number of players : '))
 
 for i in range(number_players):
@@ -26,6 +29,10 @@ with open('dictionary.txt') as f:
 
 while True:
     # choose a random word from the word_pool
+    temp = copy.deepcopy(d)
+    # lives_temp = copy.deepcopy(lives)
+    print(temp)
+
     for i in range(100):
         print()
     total_number_words = len(word_pool)
@@ -34,20 +41,31 @@ while True:
     # print out the dash mat for guessing
     word_length = len(random_word)
     guesser_container = ['-' for i in range(word_length)]
-    print(*guesser_container)
-    print()
+    # print(*guesser_container)
+    # print()
 
     # set lives to 5
     
 
-    lives = 5
+    lives = {}
     keys = list(d.keys())
+    for key in keys:
+        lives[key] = 3
+    end = 0
+    lives_temp = copy.deepcopy(lives)
     while True:
+        keys = list(d.keys())
         for key in keys:
             while True:
-                if d[key] > 5 : 
-                    print('Currently you earned : ' , d[key]//5, 'coins')
-                alphabet = input('Enter your guess alphabet/word : ')
+                print()
+                print(*guesser_container)
+                
+                if lives[key] == 0:
+                    break
+
+                if d[key] >= 5 : 
+                    print(key,'Currently you earned : ' , d[key]//5, 'coins')
+                alphabet = input(key+' Enter your guess alphabet/word : ')
                 
                 # if the user only entered 1 letter
                 if len(alphabet) == 1:
@@ -58,42 +76,63 @@ while True:
                                 guesser_container[i] = alphabet
                     else:
                         print('Wrong guess ! Guess Again.')
-                        lives-=1
-                        print('Remaining Lives : ', lives)
+                        lives[key]-=1
+                        print('Remaining Lives : ', lives[key])
+                        break
                 else:
                     # when user wants to key in the whole word
                     if random_word == alphabet:
                         print('-------You Won ------')
                         d[key] +=1
                         print('The answer is : ', random_word)
-                        print("Your Current Score is : ", score)
+                        print("Your Current Score is : ", d[key])
                         for i in range(3,0,-1):
                             print('Next question in',i+1,'seconds')
                             time.sleep(1)
                         break
                     else:
                         print('Wrong guess ! Guess Again.')
-                        lives-=1
-                        print('Remaining Lives : ', lives)
+                        lives[key]-=1
+                        print('Remaining Lives : ', lives[key])
+                        break
 
                         
                 
-                print(*guesser_container)
-                # break when all the dash are filled up
-                if '-' not in  guesser_container:
-                    d[key] +=1
-                    print('-------You Won ------')
-                    print('The answer is : ', random_word)
-                    print("Your Current Score is : ", score)
-                    for i in range(3,0,-1):
+            # print(*guesser_container)
+            # break when all the dash are filled up
+            if '-' not in  guesser_container:
+                d[key] +=1
+                print('-------You Won ------')
+                print('The answer is : ', random_word)
+                print("Your Current Score is : ", d[key])
+                for i in range(3,0,-1):
+                    print('Next question in',i+1,'seconds')
+                    time.sleep(1)
+                end = len(d)
+                break
+            
+            if lives[key] == 0:
+                end += 1
+                
+                print("----You Lost No more Lives -----")
+                # print('The answer is : ', random_word)
+                print(key," Your Current Score is : ", d[key])
+                print()
+                d.pop(key)
+                lives.pop(key)
+                # for i in range(3,0,-1):
+                #         print('Next Person continue in',i+1,'seconds')
+                #         time.sleep(1)
+                break
+        # print(end,len(d))
+        if len(d) == 0:
+            print('The answer is : ', random_word)
+            for i in range(3,0,-1):
                         print('Next question in',i+1,'seconds')
                         time.sleep(1)
-                    break
-                if lives == 0:
-                    print("----You Lost No more Lives -----")
-                    print('The answer is : ', random_word)
-                    print("Your Current Score is : ", score)
-                    for i in range(3,0,-1):
-                            print('Next question in',i+1,'seconds')
-                            time.sleep(1)
-                    break
+            print('breaking loop .......')
+            d = copy.deepcopy(temp)
+            lives = copy.deepcopy(lives_temp)
+            break
+
+    
